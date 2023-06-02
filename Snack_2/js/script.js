707640463079ba3bfe61425396f7a2c249a71a44
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-02 15:02:38 +0200

    [FEAT]lavoro array con .filter

--- Snack_2/js/script.js
+++ Snack_2/js/script.js
@@ -43,9 +43,39 @@ console.log(student)
 // utilizzo map per creare un nuovo array con i nomi degli studenti con solo lettere maiuscole
 let list = student.map((item)=>{
     nome = item.name
-    console.log(nome)
+    // console.log(nome)
     let nameUpper = nome.toUpperCase()
     return nameUpper
 })
 
 console.log(list)
+
+// utilizzo .filter per estrarre solamente gli oggetti con il valore di grades > 70
+let second_list = student.filter((item)=>{ 
+    if (item.grades > 70) {
+        return true
+    }
+})
+console.log(second_list)
+
+let nameSecondList=second_list.map((item)=>{
+    nome = item.name
+    return nome
+})
+
+console.log(nameSecondList)
+
+// utilizzo .filter per estrarre solamente gli oggetti con il valore di grades > 70 e id >120
+let third_list = student.filter((item)=>{ 
+if ((item.grades > 70) && (item.id > 120)) {
+        return true
+    }
+})
+console.log(third_list)
+
+let nameThirdList=third_list.map((item)=>{
+    nome = item.name
+    return nome
+})
+
+console.log(nameThirdList)
