9dad4d11d6b42663c585b40bf0451ddd89e6cdff
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-05 17:03:49 +0200

    [FEAT] creazione array squadre

---
+++ Snack_4/js/script.js
@@ -0,0 +1,42 @@
+// creo l'array per le squadre di calcio
+
+const footballTeam = [
+    {
+        'nome' : 'Milan',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+    {
+        'nome' : 'Inter',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+    {
+        'nome' : 'Juventus',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+    {
+        'nome' : 'Torino',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+    {
+        'nome' : 'Lazio',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+    {
+        'nome' : 'Roma',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+    {
+        'nome' : 'Fiorentina',
+        'puntiFatti' : 0,
+        'falliSubiti' : 0,
+    },
+]
+
+console.log(footballTeam)
+
