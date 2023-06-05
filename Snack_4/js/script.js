c31816180033a2e00dbb8d3f13373500d7df2760
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-05 17:13:17 +0200

    [FEAT] riempimento array numeri casuali

--- Snack_4/js/script.js
+++ Snack_4/js/script.js
@@ -40,3 +40,21 @@ const footballTeam = [
 
 console.log(footballTeam)
 
+// creo la funzione per generare un numero randomico
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min +1) ) + min;
+} 
+
+// creo un nuovo array a cui fornisco i numeri generati randomicamente
+const newScore = footballTeam.map((element )=>{
+    element.falliSubiti = randomNumber(5,50)
+    element.puntiFatti = randomNumber(2,20)
+
+    return element
+    
+})
+
+console.log(newScore)
+
+
+// creo un nuovo array
